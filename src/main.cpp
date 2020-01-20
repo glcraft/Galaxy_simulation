54a7ad337cd3559f2a6a4fa4c8c62baf7a3f1d94
Gabin Lefranc <gabin.lefranc@gmail.com>
2020-01-20 19:46:31 +0100

    Correction encodage

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,24 @@ SDL_Renderer* renderer = NULL;
 
 int main(int argc, char* argv[])
 {
-
-
-	// ------------------------- Paramùtres de la simulation -------------------------
-
-
-
-	Float	area = 500.;				// Taille de la zone d'apparition des ùtoiles (en annùes lumiùre)
+	// ------------------------- Param√®tres de la simulation -------------------------
+	Float	area = 500.;				// Taille de la zone d'apparition des √©toiles (en ann√©es lumi√®re)
 	Float	galaxy_thickness = 0.05;	// Epaisseur de la galaxie (en "area")
-	Float	precision = 0.5;			// Prùcision du calcul de l'accùlùration (algorithme de BarnesùHut)
-	bool	verlet_integration = true;	// Utiliser l'intùgration de Verlet au lieu de la mùthode d'Euler
+	Float	precision = 0.5;			// Pr√©cision du calcul de l'acc√©l√©ration (algorithme de Barnes-Hut)
+	bool	verlet_integration = true;	// Utiliser l'int√©gration de Verlet au lieu de la m√©thode d'Euler
 
-	int		stars_number = 30000;		// Nombre d'ùtoiles (Limitù ù 30 000 par les std::vector<>)
-	Float	initial_speed = 2500.;		// Vitesse initiale des d'ùtoiles (en mùtres par seconde)
+	int		stars_number = 30000;		// Nombre d'√©toiles (Limit√© √† 30 000 par les std::vector<>)
+	Float	initial_speed = 2500.;		// Vitesse initiale des d'√©toiles (en m√™tres par seconde)
 	Float	black_hole_mass = 0.;		// Masse du trou noir (en masses solaires)
-	bool	is_black_hole = false;		// Prùsence d'un trou noir
+	bool	is_black_hole = false;		// Pr√©sence d'un trou noir
 
 	View	view = xy;		// Type de vue
 	Float	zoom = 800.;				// Taille de "area" (en pixel)
-	Float	real_colors = false;		// Activer la couleur rùelle des ùtoiles
+	Float	real_colors = false;		// Activer la couleur r√©elle des √©toiles
 	bool	show_blocks = false;		// Afficher les blocs
 
-	Float	step = 200000.;				// Pas de temps de la simulation (en annùes)
+	Float	step = 200000.;				// Pas de temps de la simulation (en ann√©es)
 	time_t	simulation_time = 3600;		// Temps de simulation (en seconde)
-
-
-
 	// -------------------------------------------------------------------------------
 
 
@@ -89,7 +81,7 @@ int main(int argc, char* argv[])
 	{
 		create_blocks(area, blocks, galaxy, blocks_temp);
 
-		for (int i = 0; i < galaxy.size(); i++) // Boucle sur les ùtoiles de la galaxie
+		for (int i = 0; i < galaxy.size(); i++) // Boucle sur les √©toiles de la galaxie
 		{
 			if (galaxy[i].is_alive)
 			{
